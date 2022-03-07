61ce2717d45a8189f61013e2f993a9c9d9cc326e
goranii <chanpoong@naver.com>
2022-03-07 11:21:32 +0900

    Class Dynamic Routing

--- class_build/pages/boards/[id]/index.js
+++ class_build/pages/boards/[id]/index.js
@@ -1,7 +1,9 @@
+import { useRouter } from "next/router";
 export default function BoardDetailPage() {
+  const router = useRouter();
   return (
     <div>
-      <h1>BoardDetailPage</h1>
+      <h1>BoardDetailPage, 게시글 아이디는 {router.query.boardId}</h1>
     </div>
   );
 }
--- freeboard_frontend/pages/src/components/units/mypage/productIPicked/productIPicked.container.tsx
+++ freeboard_frontend/pages/src/components/units/mypage/productIPicked/productIPicked.container.tsx
@@ -14,9 +14,7 @@ export default function ProductIPickedPage() {
   const { data, refetch } = useQuery(FETCH_USED_ITEMS_I_PICKED, {
     variables: { page: 1, search: "" },
   });
-  const { data: CountOfIPicked, refetch: RefetchCountIPicked } = useQuery(
-    FETCH_USED_ITEMS_COUNT_I_PICKED
-  );
+  const { data: CountOfIPicked } = useQuery(FETCH_USED_ITEMS_COUNT_I_PICKED);
 
   const [toggleUseditemPick] = useMutation(TOGGLE_USED_ITEM_PICK);
 
@@ -43,36 +41,30 @@ export default function ProductIPickedPage() {
     <div>
       <h1>장바구니</h1>
       <div>
-        {data?.fetchUseditemsIPicked.map((el) =>
-          el.soldAt ? (
-            ""
-          ) : (
-            <div key={uuidv4}>
-              <button id={el._id} onClick={pickProduct}>
-                장바구니에서 삭제
-              </button>
+        {data?.fetchUseditemsIPicked.map((el) => (
+          <div key={uuidv4}>
+            <button id={el._id} onClick={pickProduct}>
+              장바구니에서 삭제
+            </button>
 
-              <div>
-                상품명{el.name} {el.soldAt ? "[판매완료]" : ""}
-              </div>
-              <div>
-                {el.images ? (
-                  <img
-                    style={{ maxHeight: 150, maxWidth: 150 }}
-                    src={`https://storage.googleapis.com/${el.images?.[0]}`}
-                  />
-                ) : (
-                  <img src="/img/commonImg/nolza.jpeg" />
-                )}
-              </div>
-              <div>내용{el.contents}</div>
-              <div>한줄평{el.remarks}</div>
-              <div>가격{el.price}</div>
-              <div>등록일 {getMyDate(el.createdAt)}</div>
-              <br />
+            <div>상품명{el.name}</div>
+            <div>
+              {el.images ? (
+                <img
+                  style={{ maxHeight: 150, maxWidth: 150 }}
+                  src={`https://storage.googleapis.com/${el.images?.[0]}`}
+                />
+              ) : (
+                <img src="/img/commonImg/nolza.jpeg" />
+              )}
             </div>
-          )
-        )}
+            <div>내용{el.contents}</div>
+            <div>한줄평{el.remarks}</div>
+            <div>가격{el.price}</div>
+            <div>등록일 {getMyDate(el.createdAt)}</div>
+            <br />
+          </div>
+        ))}
         <Pagination
           defaultCurrent={1}
           total={CountOfIPicked?.fetchUseditemsCountIPicked}
